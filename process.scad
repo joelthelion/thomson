8081f6290bd9260d9e7879836fb7c30745720b8d
Joël Schaerer <joel.schaerer@laposte.net>
2020-10-05 00:32:45 +0200

    nice shapes

---
+++ process.scad
@@ -0,0 +1,7 @@
+difference() {
+union() {
+    include<spheres.scad>;
+};
+//translate([0,0,5]) cube(10,true);
+sphere(4.5,$fn=30);
+}
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::io::prelude::*;
 
 const BIG_SPHERE : f32 = 2.;
 const SPHERES : usize = 79;
-const SPHERE_SIZE : f32 = 0.6;
+const SPHERE_SIZE : f32 = 1.2;
 
 struct Node {
     node : SceneNode,
@@ -30,11 +30,13 @@ fn relax(nodes: &mut Vec<Node>, brown:f32) {
         // let mut delta : Vector3<f32> = Vector3::zeros();
         let mut delta : Vector3<f32> = rnd_vec()*brown;
         delta += 0.5 *(BIG_SPHERE-x.norm()) * x / x.norm();
+        // delta += 0.2 *(-x.norm()) * x / x.norm();
         for j in 0..nodes.len() {
             let y = nodes[j].node.data().local_translation().vector;
             let dist = (x-y).norm();
             if dist != 0. {
-                delta += (0.005*(x-y)) * nodes[i].weight * nodes[j].weight / (dist*dist);
+                // delta += (0.02*(x-y)) * nodes[i].weight * nodes[j].weight / (dist*dist);
+                delta += (0.05*(x-y)) * nodes[j].weight / (dist*dist);
             }
         }
         // println!("{}: {}", i, delta);
@@ -65,7 +67,7 @@ impl State for AppState {
 }
 
 fn relaxation_scheme(i:usize) -> f32 {
-    let factor = 0.5 * f32::exp(-(i as f32) * 0.00005);
+    let factor = 0.5 * f32::exp(-(i as f32) * 0.00003);
     if factor > 0.0003 {
         factor }
     else { 0. }
@@ -79,7 +81,7 @@ fn main() {
 
     let mut nodes : Vec<Node> = Vec::new();
     for i in 0..SPHERES {
-        let weight : f32 = if i<6  { 4. } else { 1. };
+        let weight : f32 = if i<12  { 10. } else { 1. };
         let mut c = node_group.add_sphere(SPHERE_SIZE*weight.powf(0.33));
         c.set_color(1.0, 0.0, 0.0);
         let mut pos = rnd_vec();
@@ -105,7 +107,7 @@ fn main() {
             i+=1;
         }
         println!("{}", i);
-        if i>400_000 {
+        if i>150_000 {
             break;
         }
     }
