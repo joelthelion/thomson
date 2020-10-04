432d9da6076cad8982cbbdf7de0da1da9bbea153
Joël Schaerer <joel.schaerer@laposte.net>
2020-10-04 18:25:01 +0200

    relaxation

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,30 @@ use kiss3d::window::{State, Window};
 use na::{UnitQuaternion, Vector3, Translation3};
 use rand::prelude::*;
 
+const SPHERE_SIZE : f32 = 30.;
+
 fn rnd_vec() -> Vector3<f32> {
     let mut rng = rand::thread_rng();
     Vector3::new(rng.gen::<f32>()-0.5,rng.gen::<f32>()-0.5,rng.gen::<f32>()-0.5)
+}
+
+fn relax(cubes: &mut Vec<SceneNode>) {
+    for i in 0..cubes.len() {
+        let x = cubes[i].data().local_translation().vector;
+        let mut delta : Vector3<f32> = Vector3::zeros();
+        for j in 0..cubes.len() {
+            let y = cubes[j].data().local_translation().vector;
+            let dist = (x-y).norm();
+            if dist != 0. {
+                delta += (0.1*(x-y)) / (dist*dist);
+            }
+        }
+        // println!("{}: {}", i, delta);
+        let mut new_point = x+delta;
+        new_point.unscale_mut(new_point.norm() / SPHERE_SIZE);
+        delta = new_point - x;
+        cubes[i].append_translation(&Translation3::from(delta));
+    }
 }
 
 struct AppState {
@@ -40,19 +61,22 @@ fn main() {
         }
         let mut pos = rnd_vec();
         pos.unscale_mut(pos.norm());
-        pos.scale_mut(30.);
+        pos.scale_mut(SPHERE_SIZE);
         c.set_local_translation(Translation3::from(pos));
         cubes.push(c);
     }
 
     window.set_light(Light::StickToCamera);
 
-    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+    let small_rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.004);
+    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), -0.014);
 
     while window.render() {
+        cube_group.prepend_to_local_rotation(&small_rot);
         for c in cubes.iter_mut() {
             c.prepend_to_local_rotation(&rot);
         }
+        relax(&mut cubes);
     }
 
 }
