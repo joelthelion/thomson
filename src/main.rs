7f5a31cac4392c73567041cacbfb5332cc479013
Joël Schaerer <joel.schaerer@laposte.net>
2020-10-04 13:59:03 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+extern crate kiss3d;
+extern crate nalgebra as na;
+extern crate rand;
+
+use kiss3d::light::Light;
+use kiss3d::scene::SceneNode;
+use kiss3d::window::{State, Window};
+use na::{UnitQuaternion, Vector3, Translation3};
+use rand::prelude::*;
+
+fn rnd_vec() -> Vector3<f32> {
+    let mut rng = rand::thread_rng();
+    Vector3::new(rng.gen::<f32>()-0.5,rng.gen::<f32>()-0.5,rng.gen::<f32>()-0.5)
+}
+
+struct AppState {
+    c: SceneNode,
+    rot: UnitQuaternion<f32>,
+}
+
+impl State for AppState {
+    fn step(&mut self, _: &mut Window) {
+        self.c.prepend_to_local_rotation(&self.rot)
+    }
+}
+
+fn main() {
+    let mut window = Window::new("Kiss3d: wasm example");
+
+    let mut cubes : Vec<SceneNode> = Vec::new();
+    for i in 0..100 {
+        let mut c = window.add_cube(1.0, 1.0, 1.0);
+        if i%2==0 {
+            c.set_color(1.0, 0.0, 0.0);
+        } else {
+            c.set_color(1.0, 1.0, 1.0);
+        }
+        let mut pos = rnd_vec();
+        pos.scale_mut(30.);
+        c.set_local_translation(Translation3::from(pos));
+        cubes.push(c);
+    }
+
+    window.set_light(Light::StickToCamera);
+
+    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+
+    while window.render() {
+        for c in cubes.iter_mut() {
+            c.prepend_to_local_rotation(&rot);
+        }
+    }
+
+}
